764e65dcf228720b5ce76f92cfaceaee920b43db
Magnus Hagander <magnus@hagander.net>
2012-06-21 11:22:13 +0200

    Oops, missed to git-add a file earlier

---
+++ loader/lib/log.py
@@ -0,0 +1,33 @@
+class Log(object):
+	def __init__(self):
+		self.verbose = False
+
+	def set(self, verbose):
+		self.verbose = verbose
+
+	def status(self, msg):
+		if self.verbose:
+			print msg
+
+	def log(self, msg):
+		print msg
+
+	def error(self, msg):
+		print msg
+
+	def print_status(self):
+		opstatus.print_status()
+
+class OpStatus(object):
+	def __init__(self):
+		self.stored = 0
+		self.dupes = 0
+		self.failed = 0
+
+	def print_status(self):
+		print "%s stored, %s dupes, %s failed" % (self.stored, self.dupes, self.failed)
+
+
+log = Log()
+opstatus = OpStatus()
+
