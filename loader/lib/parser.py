655ac33b5b24c06fa91b72c51b819cbcd8b2971d
Magnus Hagander <magnus@hagander.net>
2012-07-01 12:07:10 +0200

    A better (?) way than writing .lower() on every single line..

--- loader/lib/parser.py
+++ loader/lib/parser.py
@@ -52,19 +52,20 @@ class ArchivesParser(object):
 
 
 	def clean_charset(self, charset):
-		if charset.lower() == 'unknown-8bit' or charset.lower() == 'x-unknown':
+		lcharset = charset.lower()
+		if lcharset == 'unknown-8bit' or lcharset == 'x-unknown':
 			# Special case where we don't know... We'll assume
 			# us-ascii and use replacements
 			return 'us-ascii'
-		if charset.lower() == 'x-gbk':
+		if lcharset == 'x-gbk':
 			# Some MUAs set it to x-gbk, but there is a valid
 			# declaratoin as gbk...
 			return 'gbk'
-		if charset.lower() == 'iso-8859-8-i':
+		if lcharset == 'iso-8859-8-i':
 			# -I is a special logical version, but should be the
 			# same charset
 			return 'iso-8859-8'
-		if charset.lower() == 'iso-850':
+		if lcharset == 'iso-850':
 			# Strange spelling of cp850 (windows charset)
 			return 'cp850'
 		return charset
