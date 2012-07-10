6e4cfa01b1d05a161d8f8ce86b5e1bbbb3d3b552
Magnus Hagander <magnus@hagander.net>
2012-07-10 19:20:32 +0200

    Fix email hiding, using a bit more complex regex to actually find the email addresses
    (pattern shamelessly stolen from mhonarc)

--- django/archives/mailarchives/templatetags/pgfilters.py
+++ django/archives/mailarchives/templatetags/pgfilters.py
@@ -2,12 +2,23 @@ from django.template.defaultfilters import stringfilter
 from django import template
 from email.utils import parseaddr
 
+import re
+
 register = template.Library()
+
+def _rewrite_email(value):
+	return value.replace('@', '(at)').replace('.','(dot)')
 
 @register.filter(name='hidemail')
 @stringfilter
 def hidemail(value):
-	return value.replace('@', '(at)').replace('.','(dot)')
+	return _rewrite_email(value)
+
+_re_mail = re.compile('[^()<>@,;:\/\s"\'&|]+@[^()<>@,;:\/\s"\'&|]+')
+@register.filter(name='hideallemail')
+@stringfilter
+def hideallemail(value):
+	return _re_mail.sub(lambda x: _rewrite_email(x.group(0)), value)
 
 @register.filter(name='nameonly')
 @stringfilter
