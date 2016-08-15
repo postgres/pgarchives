e2b09e8092f73f371430ea9b00fe5a36a232b923
Magnus Hagander <magnus@hagander.net>
2016-08-15 12:17:56 +0200

    Add script to purge emails from varnish
    This is typically needed when manually editing a message such as setting
    the hidden reason.
    
    We should perhaps extend this at some point to support doing the hiding
    itself, but for now that is still done manually and you just pass the
    messageid to this script to clear it from the varnish caches.

---
+++ loader/purge_frontend_message.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+# purge_frontend_message.py - issue varnish purge for the message
+# in question, to for example force an expire of a hidden message.
+#
+
+import os
+import sys
+
+from optparse import OptionParser
+from ConfigParser import ConfigParser
+
+import psycopg2
+
+from lib.varnish import VarnishPurger
+
+if __name__ == "__main__":
+	optparser = OptionParser()
+	optparser.add_option('-m', '--msgid', dest='msgid', help='Messageid to load')
+
+	(opt, args) = optparser.parse_args()
+
+	if (len(args)):
+		print "No bare arguments accepted"
+		optparser.print_help()
+		sys.exit(1)
+
+	if not opt.msgid:
+		print "Message-id must be specified"
+		optparser.print_help()
+		sys.exit(1)
+
+	cfg = ConfigParser()
+	cfg.read('%s/archives.ini' % os.path.realpath(os.path.dirname(sys.argv[0])))
+	try:
+		connstr = cfg.get('db','connstr')
+	except:
+		connstr = 'need_connstr'
+
+	conn = psycopg2.connect(connstr)
+	curs = conn.cursor()
+
+	curs.execute("SELECT id, threadid FROM messages WHERE messageid=%(msgid)s", {
+		'msgid': opt.msgid,
+	})
+	id, threadid = curs.fetchone()
+
+	VarnishPurger(cfg).purge([int(threadid), ])
+	conn.close()
