e6d8cb0266601cd1e783685fe055e0cbf700f7b6
Magnus Hagander <magnus@hagander.net>
2019-01-04 15:58:55 +0100

    Get rid of inline javascript
    This moves the javascript out to the main.js file like we already had
    done for the main website.
    
    This also removes attempts to use the selectmenu plugin for the
    dropdown. This has not worked since the new design was put in place,
    because the appropriate scripts were simply never loaded. As people
    seems to be happy with that, stop *trying* to load them causing
    javascript failures on every webpage.

--- django/media/js/main.js
+++ django/media/js/main.js
@@ -3,3 +3,37 @@ $(document).ready(function() {
     $(".navbar").toggleClass("compressed", $(window).scrollTop() >= 20);
   });
 });
+
+
+$(function(){
+    /* Callback from main message view when a message is picked in dropdown */
+    $('#thread_select').change(function(e) {
+	document.location.href = '/message-id/' + $(this).val();
+    });
+
+
+    /*
+     * For flat message view, redirect to the anchor of the messageid we're watching,
+     * unless we happen to be the first one.
+     */
+    $('#flatMsgSubject[data-isfirst=False]').each(function(i, e) {
+	if (document.location.href.indexOf('#') < 0) {
+	    document.location.href = document.location.href + '#' + $(e).data('msgid');
+	    return;
+	}
+    });
+});
+
+
+
+/*
+ * Google analytics
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1345454-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
